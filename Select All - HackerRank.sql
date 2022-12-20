50c738650d4b3e5bf81cf9580ea01c7379a60711
legit0108 <legitgamer2345@gmail.com>
2022-12-20 15:32:50 +0530

    Select All - HackerRank

---
+++ Select All - HackerRank.sql
@@ -0,0 +1,7 @@
+-- Solution - 1 : Without using * wildcard operator
+
+SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION FROM CITY;
+
+-- Solution - 2 : Using * wildcard operator
+
+SELECT * FROM CITY;
