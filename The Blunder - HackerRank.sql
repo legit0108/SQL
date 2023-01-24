74eb34785dde5f45d8c830de51b0f88439070361
legit0108 <legitgamer2345@gmail.com>
2023-01-24 18:56:38 +0530

    The Blunder - HackerRank

--- The Blunder - HackerRank.sql
+++ The Blunder - HackerRank.sql
@@ -1,3 +1,3 @@
--- Use CEIL() to round the result up to the next integer and REPLACE to replace all the zeros
+-- Use CEIL() to round the result up to the next integer and REPLACE() to replace all the zeros
 
 SELECT CEIL((AVG(SALARY) - AVG(REPLACE(SALARY, '0', '')))) FROM EMPLOYEES; 
